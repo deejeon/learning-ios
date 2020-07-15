66ac638bcad540276c9189dd6e849c7d1c4acaca
deejeon <61816883+deejeon@users.noreply.github.com>
2020-07-15 14:50:54 -0700

    initial commit

---
+++ swift/swift/stringconcat.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var str = "Hello, playground"
+str += " you are so cool"
+
+print("The maximum value \(Int.max)")
+print("The minimum value \(Int.min)")
+
+print("The maximum value \(Int32.max)")
+print("The minimum value \(Int32.min)")
+
+print("The maximum value \(UInt32.max)")
+print("The minimum value \(UInt32.min)")
